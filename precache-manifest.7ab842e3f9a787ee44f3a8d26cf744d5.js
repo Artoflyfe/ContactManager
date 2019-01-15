9fb1236856ccd9a4efc6d27e85f76f63e2586e1a
unknown <elpgutiar@gmail.com>
2019-01-15 15:39:50 +0200

    Updates

---
+++ precache-manifest.7ab842e3f9a787ee44f3a8d26cf744d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca1efd9b0a83fa79be79",
+    "url": "/ContactManager/static/css/main.948eaa6c.chunk.css"
+  },
+  {
+    "revision": "ca1efd9b0a83fa79be79",
+    "url": "/ContactManager/static/js/main.ca1efd9b.chunk.js"
+  },
+  {
+    "revision": "d77b857ae2ef047f2087",
+    "url": "/ContactManager/static/css/1.45d02e25.chunk.css"
+  },
+  {
+    "revision": "d77b857ae2ef047f2087",
+    "url": "/ContactManager/static/js/1.d77b857a.chunk.js"
+  },
+  {
+    "revision": "fba7f2b2bf72dbe84d0c",
+    "url": "/ContactManager/static/js/runtime~main.fba7f2b2.js"
+  },
+  {
+    "revision": "a890f9c763255fc48a153161c7c89413",
+    "url": "/ContactManager/index.html"
+  }
+];
